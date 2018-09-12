e1094d7e28440ba3da0a403feadb9ca13d3ee9d8
SuperNova911 <suwhan77@naver.com>
2018-09-12 10:49:22 +0900

    ㅇㅇ

--- concat.c
+++ concat.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,14 +12,29 @@ int main(int argc, char *argv[])
     extern char *optarg;
     extern int optind, opterr, optopt;
 
-    while ((n = getopt(argc, argv, "nl:h")) != -1)
+    int number = 0, suppress = 0;
+    int pageLength = 20;
+
+    int fd;
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
+    while ((n = getopt(argc, argv, "nsol:h")) != -1)
     {
         switch (n)
         {
             case 'n':
+                number = 1;
                 printf("-n option\n");
                 break;
+            case 's':
+                suppress = 1;
+                break;
+            case 'o':
+
+                fd = open(("%s", argv[optind]), O_CREAT, mode);
+                break;
             case 'l':
+
                 printf("-l option with %s\n", optarg);
                 break;
             case 'h':
